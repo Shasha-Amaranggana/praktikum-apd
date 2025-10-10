ddd6102a269421b70b90a476526b54b61b7219e9
Shasha-Amaranggana <dindashashaam@gmail.com>
2025-10-11 02:39:09 +0800

    Program edit 1

--- post-test/post-test-apd-4/2509106063-DindaShashaA.-PT-4.py
+++ post-test/post-test-apd-4/2509106063-DindaShashaA.-PT-4.py
@@ -1,2 +1,135 @@
-kk=6
-print(kk)
+print("│││    FORM LOGIN    │││")
+
+username = "1"
+password = "1"
+print()
+print("Silakan login ke akun Anda.")
+
+while True:
+    input_un = input("Masukkan Username: ")
+    input_pw = input("Masukkan Password: ")
+    if input_un == username and input_pw == password:
+        print(f"Selamat datang {username}!")
+        break
+    elif input_un == "" or input_pw == "":
+        print("Username dan Password Anda tidak boleh kosong!")
+    elif input_un != username and input_pw == password:
+        print("Username Anda tidak diketahui! Silakan coba lagi.")
+    elif input_un == username and input_pw != password:
+        print("Password Anda tidak sesuai! Silakan coba lagi.")
+    else:
+        print("Username dan Password Anda tidak diketahui! Silakan coba lagi.")
+
+print("")
+print(50*"-")
+print("│││    INPUT DATA DARAH DONOR    │││")
+print("")
+
+A_pos = 0
+A_neg = 0
+B_pos = 0
+B_neg = 0
+Ab_pos = 0
+Ab_neg = 0
+O_pos = 0
+O_neg = 0
+jawab = "Y"
+
+while jawab == "Y":
+
+    while True:
+        jenis_goldar = input("Masukkan jenis golongan darah (A, B, AB, O): ")
+
+        # Jika A
+        if jenis_goldar == "A":
+            while True:
+                jenis_rhesus = input("Masukkan jenis Rhesus (+ atau -): ")
+                if jenis_rhesus == "+":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    A_pos += konversi_volume
+                    break
+                elif jenis_rhesus == "-":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    A_neg += konversi_volume
+                    break
+                else:
+                    print("Rhesus tidak diketahui! Isi ulang.")
+            break
+
+        # Jika B
+        elif jenis_goldar == "B":
+            while True:
+                jenis_rhesus = input("Masukkan jenis Rhesus (+ atau -): ")
+                if jenis_rhesus == "+":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    B_pos += konversi_volume
+                    break
+                elif jenis_rhesus == "-":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    B_neg += konversi_volume
+                    break
+                else:
+                    print("Rhesus tidak diketahui! Isi ulang.")
+            break
+
+        # Jika AB
+        elif jenis_goldar == "AB":
+            while True:
+                jenis_rhesus = input("Masukkan jenis Rhesus (+ atau -): ")
+                if jenis_rhesus == "+":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    Ab_pos += konversi_volume
+                    break
+                elif jenis_rhesus == "-":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    Ab_neg += konversi_volume
+                    break
+                else:
+                    print("Rhesus tidak diketahui! Isi ulang.")
+            break
+
+        # Jika O
+        elif jenis_goldar == "O":
+            while True:
+                jenis_rhesus = input("Masukkan jenis Rhesus (+ atau -): ")
+                if jenis_rhesus == "+":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    O_pos += konversi_volume
+                    break
+                elif jenis_rhesus == "-":
+                    jumlah_kantong = int(input("Masukkan jumlah kantong darah: "))
+                    konversi_volume = jumlah_kantong * 500
+                    O_neg += konversi_volume
+                    break
+                else:
+                    print("Rhesus tidak diketahui! Isi ulang.")
+            break
+
+        else:
+            print("Golongan darah tidak dikenal! Isi ulang.")
+
+    jawab = input("Apakah Anda masih mau input lagi (Y/T)? ")
+
+print("")
+print(50*"-")
+print("│││    DAFTAR HASIL DARAH YANG TERKUMPUL    │││")
+print("")
+
+print(f"• Golongan A+ = {A_pos}ml")
+print(f"• Golongan A- = {A_neg}ml")
+print(f"• Golongan B+ = {B_pos}ml")
+print(f"• Golongan B- = {B_neg}ml")
+print(f"• Golongan AB+ = {Ab_pos}ml")
+print(f"• Golongan AB- = {Ab_neg}ml")
+print(f"• Golongan O+ = {O_pos}ml")
+print(f"• Golongan O- = {O_neg}ml")
+
+print("")
+print(f"Terima kasih atas bantuannya, {username}!")
